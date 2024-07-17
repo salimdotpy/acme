556c31c693be08cd4e40485bf1ebecbe2d097e69
salimdotpy <salimdotpy@gmail.com>
2024-07-17 15:34:00 +0100

    Adding pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,5 @@
 import Pagination from '@/app/ui/invoices/pagination';
+import { fetchInvoicesPages } from '@/app/lib/data';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
@@ -16,6 +17,7 @@ export default async function Page({
   }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
 
     return (
     <div className="w-full">
@@ -30,7 +32,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
